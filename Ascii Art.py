473723ea105542089699adf3c61109eb40ebb5d6
arghyagod-coder <76429451+arghyagod-coder@users.noreply.github.com>
2021-01-25 19:51:38 +0530

    Add files via upload
    Change the Files in this Program before you use

---
+++ Ascii Art.py
@@ -0,0 +1,5 @@
+import pywhatkit
+
+pywhatkit.image_to_ascii_art('Image Link or Name (with extension)', 'Acsii Output name (any random name.txt file) ')
+
+# Now a file which you named as .txt will be created in folder. Open that file to see the output.
